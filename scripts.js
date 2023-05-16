9dad97bd9ba5abdbdac89072d30615622aa72dd9
ferramentasitalents <133061937+ferramentasitalents@users.noreply.github.com>
2023-05-16 18:45:42 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,7 @@
+function toggleAnswer(event) {
+var question = event.target;
+var answer = question.nextElementSibling;
 
+question.classList.toggle("expanded");
+answer.style.display = answer.style.display === "block" ? "none" : "block";
+}
